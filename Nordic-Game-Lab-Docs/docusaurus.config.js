1bd2113a4e873d2dcd043e763fbadfd070991b75
hi5a <okbomer999@gmail.com>
2023-05-25 14:55:03 +0000

    feat: changed docusaurus config

--- Nordic-Game-Lab-Docs/docusaurus.config.js
+++ Nordic-Game-Lab-Docs/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'Nordic Game Lab', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
@@ -38,11 +38,12 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/nordic-game-lab/documentation',
         },
         blog: {
           showReadingTime: true,
